d7a8cdfaa17402a4963bbde259ad285f4143992a
MackeyK24 <MackeyK24@gmail.com>
2018-10-15 01:57:54 -1000

    Playground Update

--- playground/worker.js
+++ playground/worker.js
@@ -1,5 +1,5 @@
 // Application Service Worker Script
-var versionStamp = 'abc6b455-fbc8-4c6c-8eca-68f943bcd1ff';
+var versionStamp = '6414601d-3017-4d99-83ec-cfb0909b77cd';
 var installFiles = ['./', './index.html', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
 
 // Install Service Worker Cache Files
@@ -55,17 +55,16 @@ self.addEventListener('fetch', function(evt) {
 
 // Detects if device is on iOS 
 isIos = function() {
-    var userAgent = navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
+  var userAgent = navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
 }
-  
+
 // Detects if device is in standalone mode
 isInStandaloneMode = function() {
     return ('standalone' in navigator) && (navigator.standalone);
 }
-  
+
 // Checks if should display install popup notification:
 if (isIos() && !isInStandaloneMode()) {
-    self.setState({ showInstallMessage: true });
+  self.setState({ showInstallMessage: true });
 }
-  
