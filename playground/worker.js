b780ae83e5bd0ce13a46d263c97d60e110c7fa8e
MackeyK24 <MackeyK24@gmail.com>
2018-10-15 00:59:41 -1000

    Playground Test

---
+++ playground/worker.js
@@ -0,0 +1,70 @@
+// Application Service Worker Script
+var versionStamp = '50c6b455-fbc8-4c6c-8eca-68f943bcd1ff';
+var installFiles = ['./', './index.html', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
+
+// Install Service Worker Cache Files
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+        caches.open(versionStamp).then(function(cache) {
+            console.log("===> Installing cache file system " + versionStamp);
+            return cache.addAll(installFiles);
+        }).then(function() {
+            return self.skipWaiting();
+        })
+    );
+});
+
+// Remove Service Worker Cache Files
+self.addEventListener('activate', function(evt) {
+    evt.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cache) {
+                    if (cache !== versionStamp) {
+                        console.log("===> Cleaning cache file system " + cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+// Service Worker Catch First Pattern
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.startsWith(self.location.origin)) {    
+        evt.respondWith(
+            caches.match(evt.request).then(function(response) {
+                return response || fetch(evt.request);
+            })
+        );
+    }
+});
+
+// Service Worker Fetch First Pattern
+// self.addEventListener('fetch', function(evt) {
+//   if (evt.request.url.startsWith(self.location.origin)) {    
+//      evt.respondWith(
+//          fetch(evt.request).catch(function() {
+//              return caches.match(evt.request);
+//          })
+//      );
+//   }
+// });
+
+// Detects if device is on iOS 
+isIos = function() {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+
+// Detects if device is in standalone mode
+isInStandaloneMode = function() {
+    return ('standalone' in window.navigator) && (window.navigator.standalone);
+}
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  self.setState({ showInstallMessage: true });
+}
