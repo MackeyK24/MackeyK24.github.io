5b94eb101c4a5fbc033b964f39bae702d9506207
MackeyK24 <MackeyK24@gmail.com>
2018-10-15 22:01:58 -1000

    Playground Update

--- playground/worker.js
+++ playground/worker.js
@@ -1,5 +1,5 @@
 // Application Service Worker Script
-var versionStamp = 'cd5404f4-3d06-4635-b3cf-cb667baa18d0';
+var versionStamp = '7e8a2f9a-b49b-46a3-95b6-2feeff067772';
 var installFiles = ['./', './index.html', './favicon.ico', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
 
 // Install Service Worker Cache Files
@@ -33,22 +33,18 @@ self.addEventListener('activate', function(evt) {
 
 // Service Worker Cache First Pattern
 self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.startsWith(self.location.origin)) {    
-        evt.respondWith(
-            caches.match(evt.request).then(function(response) {
-                return response || fetch(evt.request);
-            })
-        );
-    }
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
+        })
+    );
 });
 
 // Service Worker Fetch First Pattern
 // self.addEventListener('fetch', function(evt) {
-//   if (evt.request.url.startsWith(self.location.origin)) {    
-//      evt.respondWith(
-//          fetch(evt.request).catch(function() {
-//              return caches.match(evt.request);
-//          })
-//      );
-//   }
+//    evt.respondWith(
+//        fetch(evt.request).catch(function() {
+//            return caches.match(evt.request);
+//        })
+//    );
 // });
