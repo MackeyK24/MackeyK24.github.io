538d79e0355b66ba3a89368090713a2a1ba8550b
MackeyK24 <MackeyK24@gmail.com>
2018-10-15 20:41:17 -1000

    Playground Update

--- playground/worker.js
+++ playground/worker.js
@@ -1,5 +1,5 @@
 // Application Service Worker Script
-var versionStamp = '7cfde83b-329a-4e5a-8168-1008411288a3';
+var versionStamp = 'cd5404f4-3d06-4635-b3cf-cb667baa18d0';
 var installFiles = ['./', './index.html', './favicon.ico', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
 
 // Install Service Worker Cache Files
@@ -31,7 +31,7 @@ self.addEventListener('activate', function(evt) {
     return self.clients.claim();
 });
 
-// Service Worker Catch First Pattern
+// Service Worker Cache First Pattern
 self.addEventListener('fetch', function(evt) {
     if (evt.request.url.startsWith(self.location.origin)) {    
         evt.respondWith(
@@ -52,19 +52,3 @@ self.addEventListener('fetch', function(evt) {
 //      );
 //   }
 // });
-
-// Detects if device is on iOS 
-isIos = function() {
-  var userAgent = navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test(userAgent);
-}
-
-// Detects if device is in standalone mode
-isInStandaloneMode = function() {
-    return ('standalone' in navigator) && (navigator.standalone);
-}
-
-// Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-  self.setState({ showInstallMessage: true });
-}
