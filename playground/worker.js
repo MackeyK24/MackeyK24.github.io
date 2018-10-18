30531c2dd5e3859241c10d0002f3554b8b708795
MackeyK24 <MackeyK24@gmail.com>
2018-10-17 23:34:12 -1000

    Playground Update

--- playground/worker.js
+++ playground/worker.js
@@ -1,18 +1,15 @@
-var versionStamp = '29b91c2e-0780-424c-9509-26cd889b0870';
+var versionStamp = 'd7373a3e-5402-4fc4-ad68-eb84543e1d69';
 var installFiles = ['./', './index.html', './favicon.ico', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
-
 // Install Service Worker Cache Files
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
         caches.open(versionStamp).then(function(cache) {
-            console.log("Installing Cache: " + versionStamp);
             return cache.addAll(installFiles);
         }).then(function() {
             return self.skipWaiting();
         })
     );
 });
-
 // Remove Service Worker Cache Files
 self.addEventListener('activate', function(evt) {
     evt.waitUntil(
@@ -20,7 +17,6 @@ self.addEventListener('activate', function(evt) {
             return Promise.all(
                 cacheNames.map(function(cache) {
                     if (cache !== versionStamp) {
-                        console.log("Cleaning Cache: " + cache);
                         return caches.delete(cache);
                     }
                 })
@@ -29,29 +25,11 @@ self.addEventListener('activate', function(evt) {
     );
     return self.clients.claim();
 });
-
-// Cache Service Worker First Pattern
+// Fetch Service Worker Cache Files
 self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.indexOf('version.json') < 0) {
-        evt.respondWith(
-            caches.match(evt.request).then(function(response) {
-                return response || fetch(evt.request);
-            })
-        );
-    }
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request);
+        })
+    );
 });
-
-// Fetch Service Worker First Pattern
-// self.addEventListener('fetch', function(evt) {
-//    if (evt.request.url.indexOf('version.json') < 0) {
-//        evt.respondWith(
-//            fetch(evt.request).catch(function() {
-//                return caches.match(evt.request);
-//            })
-//        );
-//    }
-// });
-
-// On Service Worker Message Received
-// self.addEventListener('message', function(evt) {
-// });
