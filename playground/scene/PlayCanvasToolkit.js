6d1ce5a61b46afa4a2fdc19f13545940c1b4c548
MackeyK24 <MackeyK24@gmail.com>
2018-10-19 00:29:20 -1000

    Playground Update

--- playground/scene/PlayCanvasToolkit.js
+++ playground/scene/PlayCanvasToolkit.js
@@ -1,45 +1,6 @@
 ﻿if (!navigator.shaders) navigator.shaders = {};
 
 // Browser Window Services
-// Progressive Web Application
-var progressiveApp = true;
-if (progressiveApp === true && 'serviceWorker' in navigator) {
-    navigator.serviceWorker.register('worker.js', { updateViaCache: 'none' }).then(function(reg) {
-        window.registration = reg;
-        console.log('Service worker registered: ' + reg.scope);
-        // ..
-        // Check For Version Updates
-        // ..
-        var versionUrl = 'version.json?time=' + new Date().getTime().toString();
-        var versionCheck = window.hasOwnProperty('version') ? window.version : null;
-        if (versionCheck != null) {
-            fetch(versionUrl).then(function(response) {
-                if (!response.ok) throw Error(response.statusText);
-                return response.json();
-            }).then(function(versionStamp) {
-                if (versionStamp != null && versionStamp.hasOwnProperty('stamp')) {
-                    if (versionStamp.stamp !== versionCheck) {
-                        console.warn("Detected version update: " + versionStamp.stamp);
-                        if (window.registration != null) {
-                            window.registration.update();
-                            // ..
-                            // TODO: Show Pending Update Message
-                            // ..
-                            window.setTimeout(function() {
-                                alert("Pending Update Detected!!!");
-                            }, 3000);
-                        }
-                    }
-                }
-            }).catch(function(error) {
-                console.warn('Version stamp offline: ' + error);
-            });
-        }
-    }).catch(function(error) {
-        console.warn('Service worker offline: ' + error);
-    });
-}
-
 // FPS Performance Meter Panel
 var showStatsPanel = true;
 if (showStatsPanel === true && typeof FPSMeter !== "undefined") {
--- playground/worker.js
+++ playground/worker.js
@@ -1,22 +1,44 @@
-var versionStamp = 'e7ee5826-5b0a-4d70-ac3e-71281d5e1c0b';
+var versionStamp = '96d8b69d-237f-4072-b9e8-842d4baf2a4e';
 var installFiles = ['./', './index.html', './favicon.ico', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
-// Install Service Worker Cache Files
+// ..
+// Install Service Worker File System
+// ..
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
         caches.open(versionStamp).then(function(cache) {
-            return cache.addAll(installFiles);
-        }).then(function() {
-            return self.skipWaiting();
+            console.log('Installing cache: ' + versionStamp);
+            var cachePromises = installFiles.map(function(urlToPrefetch) {
+                var url = new URL(urlToPrefetch, location.href);
+                url.search += (url.search ? '&' : '?') + 'time=' + new Date().getTime().toString();
+                // ..
+                // Prefetch Service Worker Cache Files
+                // ..
+                var request = new Request(url, { mode: 'no-cors' });
+                return fetch(request, { cache: 'no-store' }).then(function(response) {
+                    if (response.status >= 400) throw new Error('request for ' + urlToPrefetch + ' failed with status ' + response.statusText);
+                    return cache.put(urlToPrefetch, response);
+                }).catch(function(error) {
+                    console.warn('Not caching ' + urlToPrefetch + ' due to ' + error);
+                });
+            });
+            return Promise.all(cachePromises).then(function() {
+                return self.skipWaiting();
+            });
+        }).catch(function(error) {
+            console.warn('Pre-Fetching Failed: ', error);
         })
     );
 });
-// Remove Service Worker Cache Files
+// ..
+// Activate Service Worker File System
+// ..
 self.addEventListener('activate', function(evt) {
     evt.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
                 cacheNames.map(function(cache) {
                     if (cache !== versionStamp) {
+                        console.log('Cleaning cache: ' + cache);
                         return caches.delete(cache);
                     }
                 })
@@ -25,13 +47,20 @@ self.addEventListener('activate', function(evt) {
     );
     return self.clients.claim();
 });
-// Fetch Service Worker Cache Files
+// ..
+// Fetch Service Worker Request Files
+// ..
 self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.indexOf('version.json') < 0) {
-        evt.respondWith(
-            caches.match(evt.request).then(function(response) {
-                return response || fetch(evt.request);
-            })
-        );
+    // Chrome Dev Tools Bug - Temporary Workaround
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=823392    
+    if (evt.request.cache === 'only-if-cached' && evt.request.mode !== 'same-origin') {
+        var oStrangeRequest = evt.request.clone();
+        console.warn('Chrome Dev Tools. Request cache has only-if-cached, but not same-origin.', oStrangeRequest.cache, oStrangeRequest.mode, 'request redirect:', oStrangeRequest.redirect, oStrangeRequest.url, oStrangeRequest);
+        return;
     }
+    evt.respondWith(
+        caches.match(evt.request).then(function(response) {
+            return response || fetch(evt.request, { cache: 'no-store' });
+        })
+    );
 });
