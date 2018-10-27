8cfa9075565631f01b6be2a87633e960a54c6ab9
MackeyK24 <MackeyK24@gmail.com>
2018-10-26 18:07:32 -1000

    Playground Update

--- playground/worker.js
+++ playground/worker.js
@@ -1,4 +1,4 @@
-var versionStamp = 'd663ac58-83d6-47a1-8244-04f35709d6de';
+var versionStamp = '93aa6a20-2a6b-4867-8264-8880a0fa3ede';
 var installFiles = ['./', './index.html', './toaster.css', './favicon.ico', './manifest.json', './scripts/CanvasTools.js', './scripts/playcanvas-ammo.js', './scripts/playcanvas-anim.js', './scripts/playcanvas-gltf.js', './scripts/playcanvas-stick.js', './scripts/playcanvas-toast.js', './scripts/playcanvas-tools.js', './scripts/playcanvas-webvr.js', './scripts/playcanvas.js', './scene/PlayCanvasToolkit.js', './scene/TestScene.bin', './scene/TestScene.gltf', './scene/assets/Country_env.dds', './scene/assets/Country_negx.png', './scene/assets/Country_negy.png', './scene/assets/Country_negz.png', './scene/assets/Country_posx.png', './scene/assets/Country_posy.png', './scene/assets/Country_posz.png', './scene/assets/TestScene_Lightmap-0_comp_light.png'];
 // ..
 // Post Service Worker Version Message
